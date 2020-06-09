994138628d734f605abbc126254f52fbd3c194c5
lazicah <lazicah@gmail.com>
2020-06-09 12:17:34 +0100

    update

--- how_to_recipes/lib/main.dart
+++ how_to_recipes/lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:how_to_recipes/core/services/key_storage/key_storage_service.dart';
+import 'package:how_to_recipes/core/services/navigation/navigation_service.dart';
+import 'package:how_to_recipes/core/utils/logger.dart';
+import 'package:how_to_recipes/locator.dart';
+import 'package:how_to_recipes/ui/router..dart';
 import 'package:how_to_recipes/ui/screens/saved_recipes_screen.dart';
 import 'package:how_to_recipes/ui/screens/splash_screen.dart';
+import 'package:how_to_recipes/ui/ui_helper.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setupLogger();
+  await setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final navigationService = locator<NavigationService>();
+  final keystorage = locator<KeyStorageService>();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      home: SplashScreen(),
-      //    home: SavedRecipesScreen(),
-    );
+    return Consumer<AppStateNotifier>(
+        builder: (BuildContext context, AppStateNotifier value, Widget child) =>
+            MaterialApp(
+              title: 'Flutter Demo',
+              theme: Constants.lightTheme,
+              darkTheme: Constants.darkTheme,
+              navigatorKey: navigationService.navigatorKey,
+              onGenerateRoute: Router.generateRoute,
+              home: SplashScreen(),
+              //    home: SavedRecipesScreen(),
+            ));
   }
 }
